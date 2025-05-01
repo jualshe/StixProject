6837396e482d1d3a820712ae2a93bc629afb375a
Julia <julia@macbookpro.lan>
2025-05-01 10:00:00 -0700

    updating weeks left

--- weeks_left.py
+++ weeks_left.py
@@ -1,1 +1,12 @@
 age = input("how old are you?")
+# print (type(age))
+max_age = 90
+# print (type(max_age))
+
+age_diff = max_age- int(age)
+
+days_left = age_diff * 365
+weeks_left = age_diff * 52
+month_lef = age_diff * 12
+
+print(f"you have {days_left} days, {weeks_left} weeks and {month_lef} month left")
