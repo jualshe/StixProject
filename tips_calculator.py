d7e1f27bb56cf61b2b8bab9d041a9aa83e8e53f8
Julia <shevchenko.j@gmail.com>
2025-05-20 14:48:18 -0700

    Updated tip calculator logic

--- tips_calculator.py
+++ tips_calculator.py
@@ -5,8 +5,9 @@ people = int(input("How many people to split the bill?"))
 
 before_tip = total/people
 tip_amount = (tip_percentage/100)*before_tip
-result = (float(before_tip)+float1(tip_amount))
-result_rounded = round(result, 2)
+result = (float(before_tip)+float(tip_amount))
+result_rounded = "{:.2f}".format(result)
+# round(result, 2)
 
-message = f"Each person should pay {result} dollars"
+message = f"Each person should pay {result_rounded} dollars"
 print(message)
