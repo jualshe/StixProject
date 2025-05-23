c42a93da7b668644c1dde42ccf809a1940d9f63f
Julia <shevchenko.j@gmail.com>
2025-05-22 19:46:02 -0700

    rollercoaster height check

---
+++ height_check.py
@@ -0,0 +1,7 @@
+print ("Welcome to the rollercoaster!")
+height = int(input("what is your height in cm?"))
+
+if height >=120:
+    print ("welcome")
+else:
+    print("come when you grow up higher!")
