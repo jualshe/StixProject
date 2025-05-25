17c9981055c542474ac7501602f715b6ca3de1b0
Julia <shevchenko.j@gmail.com>
2025-05-24 23:42:06 -0700

    added tickets price

--- height_check.py
+++ height_check.py
@@ -1,7 +1,16 @@
 print ("Welcome to the rollercoaster!")
-height = int(input("what is your height in cm?"))
+height = int(input("what is your height in cm? "))
 
 if height >=120:
-    print ("welcome")
+    print ("You can ride the rollercoaster!")
+    age = int(input("how old are you? "))
+
+    if age <12:
+        print("your ticket is $5")
+    elif age <= 18:
+        print("you tiket is $7")
+    else:
+        print("your ticket is $12")
 else:
     print("come when you grow up higher!")
+
