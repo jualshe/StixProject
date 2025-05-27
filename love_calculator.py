1dd315922af6618df2619aeaffe3b75ae6fa4cc0
Julia <shevchenko.j@gmail.com>
2025-05-26 23:09:54 -0700

    1st calc version

---
+++ love_calculator.py
@@ -0,0 +1,26 @@
+print("The Love Calculator is calculating your score...")
+name1 = input("What is your name?")
+name2 = input("What is their name?")
+
+combined_names = name1 + name2
+
+names = combined_names.lower()
+one = names.count("t" and "r" and "u" and "e")
+two = names.count("l" and "o" and "v" and "e")
+print(one,two)
+print(two)
+
+
+# TRUE
+# LOVE
+# %
+#
+# For Love Scores less than 10 or greater than 90, the message should be:
+#
+# "Your score is *x*, you go together like coke and mentos."
+# For Love Scores between 40 and 50, the message should be:
+#
+# "Your score is *y*, you are alright together."
+# Otherwise, the message will just be their score. e.g.:
+#
+# "Your score is *z*."
