d4f018b72dcb49bdbf8a06893bcc249210164b2f
Julia <shevchenko.j@gmail.com>
2025-05-29 14:36:58 -0700

    improved code and added wording

--- treasure_island.py
+++ treasure_island.py
@@ -10,18 +10,19 @@ print('''__________
 print("Welcome to Treasure Island\n"
       "Your mission is to find the treasure")
 
-first = input("You are at a cross road type left or right?")
-if first == "right":
-      print("Fall into a hole. Game Over.")
-else:
-      second = input("swim or wait?")
-      if second == "swim":
-            print("Attacked by trout. Game Over.")
+first = input('You are at a cross road. Where are you going to go? Type "left" or "right"?').lower()
+if first == "left":
+      second = input('Do you choose "swim" or "wait"?').lower()
+      if second == "wait":
+            third = input("There are 3 doors in front of you and you need to choose one? red, blue or yellow?")
+            if third == "blue":
+                  print("bye bye, you've been eaten by beasts.")
+            elif third == "red":
+                  print("oh well.. you lost, you've been burned by fire.")
+
+            elif third =="yellow":
+                  print("YOU WOOOOOONN!!!!")
       else:
-            third = input("which door? red or blue or yellow")
-            if third == "blue" :
-                  print("bye bye")
-            if third == "red":
-                  print("oh well you lost")
-            else:
-                  print("YOU WOOOOON!!")
+            print("Oh no! You've been attacked by a trout. Game Over.")
+else:
+      print("You fall into a hole. Game Over.")
