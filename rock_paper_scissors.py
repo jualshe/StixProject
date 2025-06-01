c04a0afbbe8801dfdee58afc8be7eef8d34455f3
Julia <shevchenko.j@gmail.com>
2025-05-31 22:55:25 -0700

    code cleaning

---
+++ rock_paper_scissors.py
@@ -0,0 +1,12 @@
+import random
+rand_num= random.randint(1,10)
+print(rand_num)
+
+rand_float = random.uniform(1,10)
+print(rand_float)
+
+rand = random.random()
+print(rand)
+
+love_score = random.randint(1,100)
+print(f"your love score is {love_score}")
