635517e57fd6363853bedcb1bde348d7e31b8820
Julia <shevchenko.j@gmail.com>
2025-06-02 16:22:40 -0700

    implement another way

--- banker_roulette.py
+++ banker_roulette.py
@@ -1,11 +1,14 @@
 import random
 
 names_string = input ("Give me everybody's names separated by a comma!\n")
-#Angela, Ben, Jenny, Michael, Chloe
+# Angela, Ben, Jenny, Michael, Chloe
 names = names_string.split(",")
 print(names)
-random_name = random.sample(names, 1)[0]
+
+# get the total number of items in the list
+a = len(names)
+
+random_name = random.randint(0,a-1)
+person_who_will_pay= names[random_name]
 
 print(f"{random_name} is going to buy the meal today!")
-
-
