f8dc507165c199ac4c297e3b264847033f57f02f
Julia <shevchenko.j@gmail.com>
2025-06-03 19:28:34 -0700

    added letter logic

--- treasure_map.py
+++ treasure_map.py
@@ -1,20 +1,26 @@
+from ctypes.macholib.dyld import DEFAULT_LIBRARY_FALLBACK
+
 line1 = ["⬜️","️⬜️","️⬜️"]
 line2 = ["⬜️","⬜️","️⬜️"]
 line3 = ["⬜️️","⬜️️","⬜️️"]
+
 map = [line1, line2, line3]
+
 print("Hiding your treasure! X marks the spot.")
+
 position = input() # Where do you want to put the treasure?
-user_input = position.split(",")
-print(user_input)
 
-coordinates = len(user_input)
-print(coordinates)
+x_axis  = position[0]
+if x_axis == "A":
+    x_axis = 0
+elif x_axis == "B":
+    x_axis = 1
+else:
+    x_axis = 2
 
-coordinates_row = coordinates[0]
-coordinates_column = coordinates[1]
+y_axis = int(position[1])
 
-
-
-
+selected_row = map[y_axis-1]
+selected_row[x_axis] = "X"
 
 print(f"{line1}\n{line2}\n{line3}")
