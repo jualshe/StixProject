74fc8855cfe62bd06d77daa713b544d5dcb71fe9
Julia <shevchenko.j@gmail.com>
2025-06-03 21:54:17 -0700

    treasure map done

--- treasure_map.py
+++ treasure_map.py
@@ -1,26 +1,30 @@
-from ctypes.macholib.dyld import DEFAULT_LIBRARY_FALLBACK
-
 line1 = ["⬜️","️⬜️","️⬜️"]
 line2 = ["⬜️","⬜️","️⬜️"]
 line3 = ["⬜️️","⬜️️","⬜️️"]
 
 map = [line1, line2, line3]
 
-print("Hiding your treasure! X marks the spot.")
+print("Hiding your treasure! X marks the spot. A,B,C and 1,2,3")
 
 position = input() # Where do you want to put the treasure?
 
-x_axis  = position[0]
-if x_axis == "A":
+x_axis  = position[0].lower()
+if x_axis == "a":
     x_axis = 0
-elif x_axis == "B":
+elif x_axis == "b":
     x_axis = 1
 else:
     x_axis = 2
 
 y_axis = int(position[1])
 
-selected_row = map[y_axis-1]
-selected_row[x_axis] = "X"
+# another option
+# letter = position[0].lower()
+# abc = ["a", "b", "c"]
+# letter_index = abc.index(letter)
+# number_index = int(position[1]) - 1
+#map[number_index][letter_index] = "X"
+
+map[y_axis-1] [x_axis] = "X"
 
 print(f"{line1}\n{line2}\n{line3}")
