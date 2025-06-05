7acd1cc08ce8cb1616614d38e07693e9791dc62e
Julia <shevchenko.j@gmail.com>
2025-06-04 22:59:22 -0700

    password intro

---
+++ pyPasswordGenerator.py
@@ -0,0 +1,7 @@
+print("Welcome to PyPassword Generator")
+
+print(input("How many letters would you like in your password?"))
+print(input("How many numbers would you like?"))
+print(input("How many symbols would you like?"))
+
+print("Here is your password: ")
