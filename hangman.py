d23367de00883093ac34aa161565c432a289a608
Julia <shevchenko.j@gmail.com>
2025-06-12 17:52:16 -0700

    choose the quiz

---
+++ hangman.py
@@ -0,0 +1,10 @@
+import random
+# generate random words
+word_list = ["baboon", "camel", "shark" ]
+
+#TODO1 - randomly choose a word from the list
+choosen_word = random.choice(word_list)
+print(choosen_word)
+
+
+#generate blanks for the letters
