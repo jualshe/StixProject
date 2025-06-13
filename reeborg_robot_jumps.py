5efcf2e6da875cf28469dad6c2c8708df6953af7
Julia <shevchenko.j@gmail.com>
2025-06-12 17:51:53 -0700

    added maze solution

--- reeborg_robot_jumps.py
+++ reeborg_robot_jumps.py
@@ -25,3 +25,21 @@ while at_goal() != True:
     else:
         move()
 
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# while front_is_clear():
+#     move()
+# turn_left()
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#     if front_is_clear():
+#         move()
+#     if wall_in_front() and wall_on_right():
+#         turn_left()
