27c0f90eec03de2a302f1191b13c79ef190d35ae
Julia <shevchenko.j@gmail.com>
2025-06-13 22:47:59 -0700

    hangman hight level

--- hangman.py
+++ hangman.py
@@ -6,5 +6,16 @@ word_list = ["baboon", "camel", "shark" ]
 choosen_word = random.choice(word_list)
 print(choosen_word)
 
+#TODO2 -  ask user to guess the letter
+guess_letter = input("enter letter: ")
+print(guess_letter)
 
+#TODO3 - check is the letter is in the word
+if guess_letter in choosen_word:
+    print("HOORAHH!")
+else:
+    print("NOOOAAAAWWWW!")
+
+
+#TODOS:
 #generate blanks for the letters
