824c21c435f9d0e46a765cf12743e4a6cc4cd02e
Julia <shevchenko.j@gmail.com>
2025-06-15 22:56:09 -0700

    improved todo3 - show result of comparison letter in a word

--- hangman.py
+++ hangman.py
@@ -8,14 +8,11 @@ print(choosen_word)
 
 #TODO2 -  ask user to guess the letter
 guess_letter = input("enter letter: ")
-print(guess_letter)
 
 #TODO3 - check is the letter is in the word
-if guess_letter in choosen_word:
-    print("HOORAHH!")
-else:
-    print("NOOOAAAAWWWW!")
 
-
-#TODOS:
-#generate blanks for the letters
+for letter in choosen_word:
+    if guess_letter == letter:
+        print(f'{letter } Right!')
+    else:
+        print(f"{letter } Wrong!")
