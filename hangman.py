c296b7497807f2855fab12c5a18cf59f7c13e56e
Julia <shevchenko.j@gmail.com>
2025-06-16 23:31:16 -0700

    underscores added and revealed letter is shown

--- hangman.py
+++ hangman.py
@@ -1,18 +1,23 @@
 import random
 # generate random words
 word_list = ["baboon", "camel", "shark" ]
+#randomly choose a word from the list
+chosen_word = random.choice(word_list)
+print(chosen_word)
 
-#TODO1 - randomly choose a word from the list
-choosen_word = random.choice(word_list)
-print(choosen_word)
+display = []
+word_length = len(chosen_word)
+#TODO-1 -print _ for each letter of the random word
+for letter in range (0, word_length):
+    display += ["_"]
+print(display)
 
-#TODO2 -  ask user to guess the letter
-guess_letter = input("enter letter: ")
+# ask user to guess the letter
+guess = input("Guess a letter: ").lower()
+#TODO-2 - for each guessed letter reveil in a list
 
-#TODO3 - check is the letter is in the word
-
-for letter in choosen_word:
-    if guess_letter == letter:
-        print(f'{letter } Right!')
-    else:
-        print(f"{letter } Wrong!")
+for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display [position] = letter
+print(display)
