8667c0fd8f7203ef6ce66994c555f1ace67d37c4
Julia <shevchenko.j@gmail.com>
2025-06-18 16:00:41 -0700

    improvements and import files

--- hangman.py
+++ hangman.py
@@ -1,96 +1,40 @@
 import random
-stages = [r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', r'''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+from hangman_art import  logo, stages
+from hangman_words import word_list
 
-word_list = ["baboon", "camel", "shark" ]
+print(logo)
+display = []
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-display = []
 word_length = len(chosen_word)
-
 for letter in range (0, word_length):
     display += ["_"]
 print(display)
 
 end_of_the_game = False
-
 lives = 6
 
 while not end_of_the_game:
     guess = input("Guess a letter: ").lower()
     lives: object
 
-
     for position in range(word_length):
         letter = chosen_word[position]
 
         if letter == guess:
             display [position] = letter
-    if guess not in chosen_word:
-            lives -=1
-            print(f"lives: {lives}")
-            print(stages[lives])
     print(display)
+
+    if guess not in chosen_word:
+        lives -=1
+        if lives == 0:
+            end_of_the_game = True
+            print("You loose!")
+        print(f"lives: {lives}")
+        print(hangman_art.stages[lives])
 
     if not "_" in display:
         end_of_the_game = True
         print("You won!")
-    if lives == 0:
-        end_of_the_game = True
-        print("You loose!")
