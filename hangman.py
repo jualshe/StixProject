091a1145862757dc72a191ce5f0f7df84016d627
Julia <shevchenko.j@gmail.com>
2025-06-17 23:37:37 -0700

    multiple guessing option

--- hangman.py
+++ hangman.py
@@ -7,17 +7,21 @@ print(chosen_word)
 
 display = []
 word_length = len(chosen_word)
-#TODO-1 -print _ for each letter of the random word
+
 for letter in range (0, word_length):
     display += ["_"]
 print(display)
 
-# ask user to guess the letter
-guess = input("Guess a letter: ").lower()
-#TODO-2 - for each guessed letter reveil in a list
+#TODO-1 - Use a while loop to let the user guess again.
+# Use a while loop to let the user guess again.
+# The loop should only stop once the user has guessed all the letters in the chosen_word.
+# At that point display has no more blanks ("_"). Then you can tell the user they've won
 
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display [position] = letter
-print(display)
+while "_" in display:
+    guess = input("Guess a letter: ").lower()
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display [position] = letter
+    print(display)
