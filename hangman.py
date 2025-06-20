7669212b994e0d45a4034188797cebe59fcc1665
Julia <shevchenko.j@gmail.com>
2025-06-20 11:59:52 -0700

    improved guesses from a wrong guesses notification

--- hangman.py
+++ hangman.py
@@ -15,29 +15,30 @@ print(display)
 
 end_of_the_game = False
 lives = 6
+wrong_guesses = []
 
 while not end_of_the_game:
     guess = input("Guess a letter: ").lower()
-    lives: object
+
+    if guess in display:
+        print(f"You've chosen {guess}. And you've already guessed {guess}.")
 
     for position in range(word_length):
         letter = chosen_word[position]
 
         if letter == guess:
-            if guess in display:
-                print(f"You've chosen {guess}. And you've already guessed {guess}.")
-            else:
-                display [position] = letter
-
+            display [position] = letter
     print(display)
 
-
-    if guess not in chosen_word:
+    if guess in wrong_guesses:
+        print(f"You've chosen {guess}. And you've already guessed {guess}. It's not in the list!")
+    elif guess not in chosen_word:
+        wrong_guesses += guess
         lives -=1
-        print(f"You guessed '{guess}' that's not in the word. You loose a life.")
+        print(f"You guessed '{guess}' that's not in the word. You lose a life.")
         if lives == 0:
             end_of_the_game = True
-            print("You loose!")
+            print("You lose!")
         print(f"lives: {lives}")
         print(stages[lives])
 
