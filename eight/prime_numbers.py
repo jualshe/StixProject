4a3a280e4e6d9a5b36fe8db4c0d29d6f9f3d744d
Julia <shevchenko.j@gmail.com>
2025-06-21 22:08:38 -0700

    prime numbers calculations

---
+++ eight/prime_numbers.py
@@ -0,0 +1,28 @@
+# def prime_checker(number):
+#     if n > 1:
+#         if n % n == 0:
+#             if n % 1 ==0:
+#
+#                 print( "prime")
+#             else:
+#                 print("not prime")
+#
+def prime_checker(number):
+    if number <= 1:
+        print("Not prime or non-prime (must be greater than 1)")
+    else:
+        is_prime = True
+        for i in range(2, number):
+            if number % i == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            print(f"{number} is prime.")
+        else:
+            print(f"{number} is non-prime.")
+
+
+
+number = int(input()) # Check this number
+prime_checker(number)
