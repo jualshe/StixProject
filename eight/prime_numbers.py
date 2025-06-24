cd218a8a7220f26c75ef488141bbc33f6f409d62
Julia <shevchenko.j@gmail.com>
2025-06-23 22:44:22 -0700

    improvements

--- eight/prime_numbers.py
+++ eight/prime_numbers.py
@@ -1,12 +1,3 @@
-# def prime_checker(number):
-#     if n > 1:
-#         if n % n == 0:
-#             if n % 1 ==0:
-#
-#                 print( "prime")
-#             else:
-#                 print("not prime")
-#
 def prime_checker(number):
     if number <= 1:
         print("Not prime or non-prime (must be greater than 1)")
@@ -16,13 +7,11 @@ def prime_checker(number):
             if number % i == 0:
                 is_prime = False
                 break
-
+                
         if is_prime:
             print(f"{number} is prime.")
         else:
             print(f"{number} is non-prime.")
-
-
 
 n = int(input()) # Check this number
 prime_checker(number = n)
