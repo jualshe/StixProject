077a4434e85cbd3dfa4507da2b2e098c045f8b24
Julia <shevchenko.j@gmail.com>
2025-06-24 22:01:55 -0700

    inputs

--- eight/cesar_cipher.py
+++ eight/cesar_cipher.py
@@ -6,8 +6,13 @@
 # greet_with_name("Brownie")
 #
 #
-def greet_with(name, location):
-    print(f"Hello {name}")
-    print(f"what is it like in  {location}?")
-greet_with("Julia", "Lviv")
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"what is it like in  {location}?")
+# greet_with("Julia", "Lviv")
 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
