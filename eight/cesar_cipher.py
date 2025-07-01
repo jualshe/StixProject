d6271619f51d01df363d4062a4ca5e447de626b8
Julia <shevchenko.j@gmail.com>
2025-06-30 23:22:39 -0700

    updaates

--- eight/cesar_cipher.py
+++ eight/cesar_cipher.py
@@ -1,16 +1,3 @@
-# def greet_with_name(name):
-#     print(f"AA " + name)
-#     print(f"BB " + name)
-#     print(f"CC " + name)
-#
-# greet_with_name("Brownie")
-#
-#
-# def greet_with(name, location):
-#     print(f"Hello {name}")
-#     print(f"what is it like in  {location}?")
-# greet_with("Julia", "Lviv")
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
