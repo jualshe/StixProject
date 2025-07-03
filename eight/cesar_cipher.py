8e2017c4064d9a34c57fb5fafea0e7c9143e2334
Julia <shevchenko.j@gmail.com>
2025-07-03 00:00:26 -0700

    decrypt

--- eight/cesar_cipher.py
+++ eight/cesar_cipher.py
@@ -1,6 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
@@ -10,16 +10,26 @@ shift = int(input("Type the shift number:\n"))
 # TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
 #  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
-
-def encrypt(plain_text, shift_amount):
-    cipher_text = ''
-    for letter in plain_text:
-        letter_index = plain_text.index(letter)
+def decrypt(cipher_text, shift_amount):
+    plain_text = ''
+    for letter in cipher_text:
+        letter_index = cipher_text.index(letter)
         position = alphabet.index(letter)
-        new_position = position + shift_amount
+        new_position = position - shift_amount
         new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
+        plain_text += new_letter
+    print(f"The decoded text is {plain_text}")
+decrypt(cipher_text=text, shift_amount=shift)
 
-encrypt(plain_text=text, shift_amount=shift)
+# def encrypt(plain_text, shift_amount):
+#     cipher_text = ''
+#     for letter in plain_text:
+#         letter_index = plain_text.index(letter)
+#         position = alphabet.index(letter)
+#         new_position = position + shift_amount
+#         new_letter = alphabet[new_position]
+#         cipher_text += new_letter
+#     print(f"The encoded text is {cipher_text}")
+#
+# encrypt(plain_text=text, shift_amount=shift)
 
