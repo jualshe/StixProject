49fae449a04859909f5a2f73358dea7ab96a587d
Julia <shevchenko.j@gmail.com>
2025-07-03 23:29:16 -0700

    if added depending on encode or decoreuser input

--- eight/cesar_cipher.py
+++ eight/cesar_cipher.py
@@ -4,11 +4,10 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-# TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
-# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
-#  by the shift amount and print the decrypted text.
 # TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
 #  Use the value of the user chosen 'direction' variable to determine which functionality to use.
+
+
 
 def decrypt(cipher_text, shift_amount):
     plain_text = ''
@@ -19,17 +18,22 @@ def decrypt(cipher_text, shift_amount):
         new_letter = alphabet[new_position]
         plain_text += new_letter
     print(f"The decoded text is {plain_text}")
-decrypt(cipher_text=text, shift_amount=shift)
+# decrypt(cipher_text=text, shift_amount=shift)
 
-# def encrypt(plain_text, shift_amount):
-#     cipher_text = ''
-#     for letter in plain_text:
-#         letter_index = plain_text.index(letter)
-#         position = alphabet.index(letter)
-#         new_position = position + shift_amount
-#         new_letter = alphabet[new_position]
-#         cipher_text += new_letter
-#     print(f"The encoded text is {cipher_text}")
-#
+def encrypt(plain_text, shift_amount):
+    cipher_text = ''
+    for letter in plain_text:
+        letter_index = plain_text.index(letter)
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
+
 # encrypt(plain_text=text, shift_amount=shift)
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+else:
+    decrypt(cipher_text=text, shift_amount=shift)
 
