baf3a4249c6618cc8f7ce5800245c176f4647ec3
Julia <shevchenko.j@gmail.com>
2025-07-14 00:04:15 -0700

    restart app if user wants

--- eight/cesar_cipher.py
+++ eight/cesar_cipher.py
@@ -1,27 +1,32 @@
 from art import logo
 print(logo)
+
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-# TODO-2: What happens if the user enters a number/symbol/space?
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-shift = shift % 26
-
-# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
-#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ''
     if cipher_direction == "decode":
         shift_amount = shift_amount * -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
-    print(f"The {cipher_direction}d decoded text is {end_text}")
-# TODO-1: what if shift is higher than amount of letters in alphabet?
-# TODO-3: Can you figure out a way to restart the cipher program?
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"The {cipher_direction}d text is: {end_text}\n")
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+again = True
+while again:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    again = input("Type 'yes' if you want to go again. Otherwise type 'no'\n").lower()
+    if again == "yes":
+        again = True
+    else:
+        break
