f0ac52b0b351d9f3b23a4fa39b0539f14dc9dd2c
Julia <shevchenko.j@gmail.com>
2025-07-22 23:26:07 -0700

    grading tasks

---
+++ nine/grading_program.py
@@ -0,0 +1,14 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99,
+  "Draco": 74,
+  "Neville": 62,
+}
+
+# TODO-1: Create an empty dictionary called student_grades.
+
+
+# TODO-2: Write your code below to add the grades to student_grades.👇
+
+print(student_grades)
