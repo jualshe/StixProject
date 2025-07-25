56d496ad6a9b3f2211dc32ccd91d864e36ce238f
Julia <shevchenko.j@gmail.com>
2025-07-24 23:00:33 -0700

    more nested

---
+++ nine/nested.py
@@ -0,0 +1,29 @@
+#nesting
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin"
+}
+
+#Nesting a list in a dictionary
+tavel_log = {
+    "France" : ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"]
+}
+
+#Nesting list and dictionary in a dictionary
+travel_log = {
+    "France" : {"cities_visited": ["Paris", "Lion", "Dijon"], "total_visits": 12},
+    "Italy": {"favorite_cities": ["Venice","Vicenza","Rome"], "total_visits": 15}
+}
+
+# nesting a dictionary insideof the list
+travel_log2 = [
+    {"country": "France",
+    "cities_visited": ["Paris", "Lion", "Dijon"],
+     "total_visits": 12
+    },
+    {"country": "Italy",
+    "cities_visited": ["Venice","Vicenza","Rome"],
+     "total_visits": 15
+    }
+]
