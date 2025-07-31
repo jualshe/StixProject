d03805e159a33fddd01a533dc0f5202e5e73d4ef
Julia <shevchenko.j@gmail.com>
2025-07-30 17:10:11 -0700

    tasks complete

--- nine/blind_auction.py
+++ nine/blind_auction.py
@@ -4,32 +4,37 @@ print(logo)
 print("Welcome to the secret auction program.")
 
 # TODO-1: Ask the user for input
-name = input("What is your name?\n")
-bid = input("What is your bid?\n")
-
 # TODO-2: Save data into dictionary {name: price}
 bidders = []
+print(bidders)
 def add_new_bidders (person, price):
     new_bidder = {
         "name": person,
         "bid": price
     }
     bidders.append(new_bidder)
+    print(bidders)
+def higher_bidder():
+    highest_bid = 0
+    winner=""
+    for name, bid in bidders.items:
+        if bid > highest_bid:
+            highest_bid = bid
+            winner = name
+            print(f"The winner is {winner} with the highest bid of ${bid}.congrats!")
 
-add_new_bidders(person=name, price=bid)
-
-
-print(bidders)
 # TODO-3: Whether if new bids need to be added
-additional_user = input("Are there more bidders? Yes/No?\n")
-if additional_user == "Yes".lower():
-    clear()
+more_users = True
+while more_users:
+    name = input("What is your name?\n")
+    bid = input("What is your bid?\n")
     add_new_bidders(person=name, price=bid)
 
-else:
-    print("quit")
-    quit()
-
+    clear()
+    more_users = input("Are there more bidders? Yes/No?\n").lower()
+    if more_users == "no":
+        print("no, ok, goodbye!")
+        more_users = False
 # TODO-4: Compare bids in dictionary
 
 
