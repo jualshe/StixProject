eb33ae4f6fb465f224fea3630e1a926b250b8a3d
Julia <shevchenko.j@gmail.com>
2025-07-31 15:36:11 -0700

    optimisation and bugfixes

--- nine/blind_auction.py
+++ nine/blind_auction.py
@@ -3,38 +3,26 @@ from art import logo
 print(logo)
 print("Welcome to the secret auction program.")
 
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-bidders = []
-print(bidders)
-def add_new_bidders (person, price):
-    new_bidder = {
-        "name": person,
-        "bid": price
-    }
-    bidders.append(new_bidder)
-    print(bidders)
-def higher_bidder():
+def find_highest_bidder(bidding_record):
     highest_bid = 0
-    winner=""
-    for name, bid in bidders.items:
-        if bid > highest_bid:
-            highest_bid = bid
-            winner = name
-            print(f"The winner is {winner} with the highest bid of ${bid}.congrats!")
-
-# TODO-3: Whether if new bids need to be added
-more_users = True
-while more_users:
-    name = input("What is your name?\n")
-    bid = input("What is your bid?\n")
-    add_new_bidders(person=name, price=bid)
-
-    clear()
-    more_users = input("Are there more bidders? Yes/No?\n").lower()
-    if more_users == "no":
-        print("no, ok, goodbye!")
-        more_users = False
-# TODO-4: Compare bids in dictionary
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
 
 
+bids = {}
+continue_bidding = True
+while continue_bidding:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        clear()
