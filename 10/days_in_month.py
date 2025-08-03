41fc532f78a46def514ae84a26d4f90aca78c66f
Julia <shevchenko.j@gmail.com>
2025-08-02 22:55:54 -0700

    completed

--- 10/days_in_month.py
+++ 10/days_in_month.py
@@ -6,9 +6,18 @@ def is_leap(year):
             else:
                 return False
         else:
-            return False
+            return True
     else:
         return False
 
-result = is_leap(int(input("Which year do you want to check?")))
-print(result)
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and month == 2:
+        return 29
+    else:
+        return month_days[month-1]
+
+year = int(input("Enter the year: "))  # Enter a year
+month = int(input("Enter the month: "))  # Enter a month
+days = days_in_month(year, month)
+print(days)
