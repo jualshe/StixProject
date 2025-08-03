928175e7cac9f804021b9eaa23338463b4f510f7
Julia <shevchenko.j@gmail.com>
2025-08-02 18:44:38 -0700

    double return

--- 10/func_output.py
+++ 10/func_output.py
@@ -1,8 +1,14 @@
-#function with outouts
+#function with outputs
+from symbol import return_stmt
+
+
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't proved the right data"
+
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
-    print(f"{formated_f_name} {formated_l_name}")
+    return f"{formated_f_name} {formated_l_name}"
 
-
-format_name("juju"," sheshe")
+formated_string = format_name(input("What is your first name?"),input("What is your last name?"))
+print(formated_string)
