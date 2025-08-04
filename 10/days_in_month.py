06381f52d1773fb58443cd54ba3d8ad0ea2d8347
Julia <shevchenko.j@gmail.com>
2025-08-03 21:27:13 -0700

    fixes and improvements

--- 10/days_in_month.py
+++ 10/days_in_month.py
@@ -11,7 +11,7 @@ def is_leap(year):
         return False
 
 def days_in_month(year, month):
-    if month > 12 or < 1:
+    if month > 12 or month < 1:
         return "Invalid month"
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     if is_leap(year) and month == 2:
--- 10/func_output.py
+++ 10/func_output.py
@@ -1,7 +1,4 @@
 #function with outputs
-
-
-
 def format_name(f_name, l_name):
     """Take a first and last name and format it to return the
         title case version of the name."""
