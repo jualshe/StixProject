280862df13bb2df0c0172ad5ff8e5cbf7fa7b101
Julia <shevchenko.j@gmail.com>
2025-08-06 23:49:28 -0700

    todos

---
+++ eleven/blackjack.py
@@ -0,0 +1,25 @@
+from art import logo
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+
+#TODO 1: logo
+wannaplay = input("Do you want to play blackjack? Type 'yay' or 'nay': " )
+
+
+#TODO 2: Deal both user and computer a starting hand of 2 random card values.
+# Your cards: random from the list. Computer's card: show one of two random ones to the user
+
+#TODO 3:Detect when computer or user has a blackjack. (Ace + 10 value card).
+
+#TODO 4:
+# more_card = input("Type 'yay' if you want another card  or 'nay' to pass: ")
+
+
+#Your final hand: [x,x]
+#Computer's final hand: [y,y]'
+# WHO wins?
+
+#wannaplay = input("Do you want to play blackjack? Type 'yay' or 'nay': " )
+
+The Ace can count as 11 or 1.
