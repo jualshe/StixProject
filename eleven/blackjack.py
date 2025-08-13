bd94af7dc6a9c291b73be5b43769db46c9e7cc58
Julia <shevchenko.j@gmail.com>
2025-08-12 17:20:19 -0700

    blackjack ready

--- eleven/blackjack.py
+++ eleven/blackjack.py
@@ -1,46 +1,91 @@
 import random
+
 from art import logo
-print(logo)
+# print(logo)
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,11,11,11,11]
+cards = [10, 10]
 
-# wannaplay = input("Do you want to play blackjack? Type 'yay' or 'nay': " )
-
-#TODO 2: Deal both user and computer a starting hand of 2 random card values.
+#Deal both user and computer a starting hand of 2 random card values.
 user_cards = random.sample(cards, 2)
 print(user_cards)
 computer_cards = random.sample(cards,2)
-print(f'[{computer_cards[0]}], [X]')
+print(computer_cards)
+# print(f'[{computer_cards[0]}], [X]')
 
-#TODO 3:Detect when computer or user has a blackjack. (Ace + 10 value card).
-if sum(computer_cards) == 21:
-    print("Blackjack! Computer won!")
-elif sum(user_cards) == 21:
-    print("Blackjack! User won!")
+def comparison():
+    if blackjack_check():
+        return
+    else:
+        if user_total == computer_total:
+            print("DRAW!")
+            # return
 
-#TODO: If an ace is drawn, count it as 11. But if the total goes over 21, count the ace as 1 instead.
-if sum(user_cards) or sum(computer_cards) > 21:
-    if 11 in user_cards:
-        11 == 1
+        elif user_total < computer_total < 22:
+            print("Computer winsooo!")
+        elif want_more_card == False:
+            print("User wins oo!")
 
-#TODO 5:
-# want_more_card = True
-more_card = input("Type 'yes' if you want another card  or 'no' to pass: ")
-# while want_more_card:
-if more_card == "no":
-    print("no cards daelt")
-    #calculate the score
+# else:
+    #     print("User wins oo!")
 
-else:
-    user_cards += random.sample(cards, 1)
-    print("one more card dealt")
-    print(user_cards)
+# is there a blackjack?
+def blackjack_check():
+    if computer_total == 21:
+        print("Blackjack! Computer won!")
+        return True
+    elif user_total == 21:
+        print("Blackjack! User won!")
+        return True
+    else:
+        return False
 
+want_more_card = True
+while want_more_card:
+    user_total = sum(user_cards)
+    computer_total = sum(computer_cards)
+    if blackjack_check():
+        want_more_card = False
+        break
 
-#Your final hand: [x,x]
-#Computer's final hand: [y,y]'
-# WHO wins?
+    def adjust_for_ace(cards):
+        while sum(cards) > 21 and 11 in cards:
+            cards[cards.index(11)] = 1
 
-#wannaplay = input("Do you want to play blackjack? Type 'yay' or 'nay': " )
+    more_card = input("Type 'yes' if you want another card  or 'no' to pass: ")
 
-#The Ace can count as 11 or 1.
+    if more_card == "no":
+        print("no more cards for the user ")
+        want_more_card = False
+
+        # computer plays
+        while sum(computer_cards) <= 17:
+            print("computer is drawing a card")
+            computer_cards += random.sample(cards,1)
+            adjust_for_ace(computer_cards)
+            computer_total = sum(computer_cards)
+            print(computer_cards)
+            print(computer_total)
+            blackjack_check()
+
+            if computer_total > 21:
+                want_more_card = False
+                print("COMPUTER LOST!")
+            else:
+                comparison()
+    else:
+        user_cards += random.sample(cards, 1)
+        adjust_for_ace(user_cards)
+        user_total = sum(user_cards)
+        blackjack_check()
+        print("one more card dealt for the user")
+        print(user_cards)
+        print(user_total)
+        if user_total > 21:
+            want_more_card = False
+            print("USER LOST!")
+        else:
+            comparison()
+
+print(f'the score is user:{user_total} vs computer{computer_total}')
