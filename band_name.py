b6b336ee5bbd527c26516cb4b3e5f0a4cdefde87
Julia <shevchenko.j@gmail.com>
2026-02-18 23:07:16 -0800

    edits

---
+++ band_name.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator!")
+town = input("What is your hometown name?")
+pet = input("What is your pet name?")
+
+print("Your band name is " + town +" " + pet +"!")
